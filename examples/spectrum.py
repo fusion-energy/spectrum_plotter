3f997db81be77076bbf50aac28b7f2652c07b2f3
Jonathan <mail@jshimwell.com>
2021-10-23 19:37:39 +0100

    made examples look different

--- examples/spectrum.py
+++ examples/spectrum.py
@@ -4,9 +4,10 @@ import numpy as np
 x1 = np.array([1, 2, 3, 4, 5, 6])
 y1 = np.array([0, 1, 1, 0.5, 0.4, 3])
 y_err1 = np.array([0.2, 0.1, 0.4, 0.1, 0.1, 0.2])
+
 x2 = np.array([1, 2, 3, 4, 5, 6])
-y2 = np.array([3, 4, 2, 5, 0.5, 2])
-y_err2 = np.array([0.2, 0.1, 0.4, 0.1, 0.1, 0.2])
+y2 = np.array([3, 4, 3, 5, 3.8, 4.1])
+y_err2 = np.array([0.2, 0.1, 0.4, 0.2, 0.1, 0.2])
 
 spectrum_with_error = {
     "test plot 1": (x1, y1, y_err1),
